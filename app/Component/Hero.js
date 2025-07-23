d42c2c91fd5521d9e72fb05be0f295b9a8f488d6
Yuvraj Singh <yuvrajsingh@Yuvrajs-MacBook-Air.local>
2025-07-23 19:45:48 +0530

    h

--- app/Component/Hero.js
+++ app/Component/Hero.js
@@ -7,7 +7,7 @@ function Hero() {
     <div className='w-full h-screen flex flex-col justify-center items-center relative '>
       {/* <StarBackground className="absolute"/> */}
         <h1 className="px-4 py-2 rounded-xl border border-white/20 bg-white/10 backdrop-blur-sm text-[#726cdbda] text-[14px] font-medium shadow-sm mb-4">Hey! My nam<span className="text-[#726cdb]">e is Yuvraj!</span></h1>
-      <h1 className='lg:text-[60px] md:text-[52px] sm:text-[48px] text-[32px] text-center md:max-w-[75%] lg:leading-18 sm:leading-15 leading-10 font-[500]'>
+      <h1 className='lg:text-[60px] md:text-[52px] sm:text-[48px] text-[30px] text-center md:max-w-[75%] lg:leading-18 sm:leading-15 leading-10 font-[500]'>
         I Build Smart Web<span className='text-[#C8C8C9]'>Pages that </span> Convert Scroll<span className='text-[#C8C8C9]'>s into Sales.</span>
         </h1>
       <p className='md:text-[18px] text-[14px] text-center md:max-w-[45%] max-w-[80%]  mt-5 text-[#C8C8C9]'>Hey, I'm Yuvraj — I create websites that look great and work even better. Let’s build something your users will love..</p>
